ea46ff6f74caf7ad7c1e4acd095a8b77faf91cea
Zakaria Fikri <zakaria.fikri@issgreppi.it>
2023-09-22 10:49:11 +0200

    Costruttore

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+class Studente
+{
+    public Studente(int matricola, string cognome, string nome)
+    {
+        Matricola = matricola;
+        Cognome = cognome;
+        Nome = nome;
+    }
+    public Studente()
+    {
+    }
+    public int Matricola { get; set; }
+    public string Cognome { get; set; }
+    public string Nome { get; set; }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world");
+        Studente s = new Studente();
+    }
+}
